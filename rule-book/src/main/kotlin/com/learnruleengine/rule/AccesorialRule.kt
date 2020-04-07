577e25910e6e7bd5f940705761544160a24c904b
Dilip Sundarraj <dilip.sundarraj@target.com>
2020-04-07 13:44:24 -0500

    added the code to return a map

--- rule-book/src/main/kotlin/com/learnruleengine/rule/AccesorialRule.kt
+++ rule-book/src/main/kotlin/com/learnruleengine/rule/AccesorialRule.kt
@@ -5,22 +5,29 @@ import com.deliveredtechnologies.rulebook.model.rulechain.cor.CoRRuleBook
 import com.learnruleengine.domain.Accessorial
 import java.util.function.Predicate
 
-class AccesorialRule : CoRRuleBook<Double>() {
+class AccesorialRule : CoRRuleBook<HashMap<String, Double>>() {
 
     override fun defineRules() {
         addRule(RuleBuilder.create()
             .withFactType(Accessorial::class.java)
-            .withResultType(Double::class.java)
+            .withResultType(Pair::class.java)
             .`when` { facts -> facts.one.weight > 50 }
-            .then { t, u -> u.value = 24.0  }
-            .stop()
+            .then { t, u ->
+                run {
+                    u.value = Pair("WEIGHT", 24.0)
+                }
+            }
             .build())
 
         addRule(RuleBuilder.create()
             .withFactType(Accessorial::class.java)
-            .withResultType(Double::class.java)
+            .withResultType(Pair::class.java)
             .`when` { facts -> facts.one.length > 48 }
-            .then { t, u -> u.value = 15.0  }
+            .then { t, u ->
+                run {
+                    u.value = Pair("HEIGHT", 15.0)
+                }
+            }
             //.stop()
             .build())
     }
